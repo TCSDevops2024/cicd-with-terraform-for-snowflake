14760d4133a30c67a2bc1bb9f16c7d17dde3898f
chandra-pexa <chandrashekaryelahanka.munikrishna@pexa.com.au>
2024-05-11 19:13:32 +1000

    repo structure

---
+++ dev/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.90.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "<your-bucket-name>e"
+    key     = "<your-key>"
+    region  = "ap-southeast-2"
+    dynamodb_table = "value"
+    encrypt = true
+
+  }
+}
+provider "snowflake" {
+  account     = "<your-account>"
+  username    = "<your-username>"
+  private_key = var.snowflake_private_key
+}
+
+module "snowflake_resources" {
+  source              = "./modules/snowflake_resources"
+  time_travel_in_days = 30
+  database            = var.database
+  env_name            = var.env_name
+}
