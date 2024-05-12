e0d1ac27e7d6b4d5ffc71b5221170ab02dfee5a0
chandra-pexa <chandrashekaryelahanka.munikrishna@pexa.com.au>
2024-05-12 20:11:26 +1000

    deploy to dev

--- prod/main.tf
+++ prod/main.tf
@@ -9,7 +9,7 @@ terraform {
     bucket         = "ap-southeast2-tf-state-891988165081"
     key            = "prod"
     region         = "ap-southeast-2"
-    dynamodb_table = "sydney-terraform-state-lock-891988165081 "
+    dynamodb_table = "sydney-terraform-state-lock-891988165081"
     encrypt        = true
 
   }
