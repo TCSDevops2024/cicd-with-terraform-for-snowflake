d2d8a344bc2396044dc0aa977f2520eeb276ea49
chandra-pexa <chandrashekaryelahanka.munikrishna@pexa.com.au>
2024-05-12 20:28:00 +1000

    deploy to dev

--- modules/snowflake_resources/grants.tf
+++ modules/snowflake_resources/grants.tf
@@ -1,38 +1,59 @@
-resource "snowflake_database_grant" "db_ro_grant" {
-  database_name = snowflake_database.tf_database.name
-  privilege     = "USAGE"
-  roles         = ["TF_READER"]
+resource "snowflake_grant_privileges_to_account_roles" "db_ro_grant" {
+  roles = ["TF_READER"]
+  privileges {
+    privilege = "USAGE"
+    on_account_object {
+      object_type = "DATABASE"
+      database_name = snowflake_database.tf_database.name
+    }
+  }
 }
 
-resource "snowflake_schema_grant" "schema_ro_grant" {
-  database_name = snowflake_database.tf_database.name
-  schema_name   = snowflake_schema.tf_schema.name
-  privilege     = "USAGE"
-  roles         = ["TF_READER"]
+resource "snowflake_grant_privileges_to_account_roles" "schema_ro_grant" {
+  roles = ["TF_READER"]
+  privileges {
+    privilege = "USAGE"
+    on_account_object {
+      object_type = "SCHEMA"
+      database_name = snowflake_database.tf_database.name
+      schema_name = snowflake_schema.tf_schema.name
+    }
+  }
 }
 
-resource "snowflake_table_grant" "table_ro_grant" {
-  database_name     = snowflake_database.tf_database.name
-  schema_name       = snowflake_schema.tf_schema.name
-  privilege         = "SELECT"
-  roles             = ["TF_READER"]
-  on_future         = true
-  with_grant_option = false
-  on_all            = false
+resource "snowflake_grant_privileges_to_account_roles"  "table_ro_grant" {
+  roles = ["TF_READER"]
+  privileges {
+    privilege = "SELECT"
+    on_account_object {
+      object_type = "TABLE"
+      database_name = snowflake_database.tf_database.name
+      schema_name = snowflake_schema.tf_schema.name
+      table_name = snowflake_table.tf_table.name
+    }
+  }
 }
 
-resource "snowflake_view_grant" "view_ro_grant" {
-  database_name     = snowflake_database.tf_database.name
-  schema_name       = snowflake_schema.tf_schema.name
-  privilege         = "SELECT"
-  roles             = ["TF_READER"]
-  on_future         = true
-  with_grant_option = false
-  on_all            = false
+resource "snowflake_grant_privileges_to_account_roles"  "view_ro_grant" {
+  roles = ["TF_READER"]
+  privileges {
+    privilege = "SELECT"
+    on_account_object {
+      object_type = "VIEW"
+      database_name = snowflake_database.tf_database.name
+      schema_name = snowflake_schema.tf_schema.name
+      view_name = snowflake_view.tf_view.name
+    }
+  }
 }
 
-resource "snowflake_warehouse_grant" "warehouse_grant" {
-  warehouse_name = snowflake_warehouse.tf_warehouse.name
-  privilege      = "USAGE"
-  roles          = ["TF_READER"]
+resource "snowflake_grant_privileges_to_account_roles"  "warehouse_grant" {
+  roles = ["TF_READER"]
+  privileges {
+    privilege = "USAGE"
+    on_account_object {
+      object_type = "WAREHOUSE"
+      warehouse_name = snowflake_warehouse.tf_warehouse.name
+    }
+  }
 }
